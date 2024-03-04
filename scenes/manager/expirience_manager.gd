e9d77d4d442d4f27d6596a52e26c677b864f6529
Marllon Freitas <71530690+Marllon-Freitas@users.noreply.github.com>
2024-03-04 15:49:59 -0300

    feat: adding base upgrade ui

--- scenes/manager/expirience_manager.gd
+++ scenes/manager/expirience_manager.gd
@@ -7,7 +7,7 @@ const TARGET_EXPERIENCE_GROWTH = 5
 
 var current_expirience: float = 0
 var current_level: int = 1
-var target_experience: float = 5
+var target_experience: float = 1
 
 
 func _ready():
--- scenes/manager/upgrade_manager.gd
+++ scenes/manager/upgrade_manager.gd
@@ -2,6 +2,7 @@ extends Node
 
 @export var upgrade_pool: Array[AbilityUpgrade]
 @export var experience_manager: Node
+@export var upgrade_screen_scene: PackedScene
 
 var current_upgrades = {}
 
@@ -15,10 +16,14 @@ func on_level_up(current_level: int):
 	if chosen_upgrade == null:
 		return
 
-	var has_upgrade = current_upgrades.has(chosen_upgrade.id)
-	if !has_upgrade:
-		current_upgrades[chosen_upgrade.id] = {"resourse": chosen_upgrade, "quantity": 1}
-	else:
-		current_upgrades[chosen_upgrade.id]["quantity"] += 1
+	var upgrade_screen_instance = upgrade_screen_scene.instantiate()
+	add_child(upgrade_screen_instance)
+	upgrade_screen_instance.set_ability_upgrades([chosen_upgrade] as Array[AbilityUpgrade])
 
-	print(current_upgrades)
+
+func aplly_upgrade(upgrade: AbilityUpgrade):
+	var has_upgrade = current_upgrades.has(upgrade.id)
+	if !has_upgrade:
+		current_upgrades[upgrade.id] = {"resourse": upgrade, "quantity": 1}
+	else:
+		current_upgrades[upgrade.id]["quantity"] += 1
