753b8c43a38ccc8a3bb85bc2092486c265519cc6
Marllon Freitas <71530690+Marllon-Freitas@users.noreply.github.com>
2024-03-04 16:37:40 -0300

    feat: making the ability work

--- scenes/abilities/sword_ability_controller/sword_ability_controller.gd
+++ scenes/abilities/sword_ability_controller/sword_ability_controller.gd
@@ -1,14 +1,17 @@
 extends Node
 
 @export var max_range_float: float = 150
-
 @export var sword_ability: PackedScene
+
 var damage: int = 5
+var base_wait_time
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	base_wait_time = $Timer.wait_time
 	$Timer.timeout.connect(on_Timer_timeout)
+	GameEvents.ability_upgrade_added.connect(on_ability_upgrade_added)
 
 
 func on_Timer_timeout():
@@ -42,3 +45,12 @@ func on_Timer_timeout():
 
 	var enemy_direction: Vector2 = enemies[0].global_position - sword_instance.global_position
 	sword_instance.rotation = enemy_direction.angle()
+
+
+func on_ability_upgrade_added(upgrade: AbilityUpgrade, current_upgrades: Dictionary):
+	if upgrade.id != "sword_rate":
+		return
+
+	var percent_reduction = current_upgrades["sword_rate"]["quantity"] * 0.1
+	$Timer.wait_time = base_wait_time * (1 - percent_reduction)
+	$Timer.start()
