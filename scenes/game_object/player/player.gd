48f467a537e9ae2a043da2ef5653e78086784865
Marllon Freitas <71530690+Marllon-Freitas@users.noreply.github.com>
2024-03-05 11:57:28 -0300

    feat: adding health bar ui

--- scenes/game_object/player/player.gd
+++ scenes/game_object/player/player.gd
@@ -4,6 +4,8 @@ const MAX_SPEED = 150
 const ACCELERATION_SMOOTHING = 25
 
 @onready var damage_interval_timer = $DamageIntervalTimer
+@onready var health_component = $HealthComponent
+@onready var health_bar = $HealthBar
 
 var number_colliding_body: int = 0
 
@@ -12,6 +14,8 @@ func _ready():
 	$CollisionArea2D.body_entered.connect(on_body_entered)
 	$CollisionArea2D.body_exited.connect(on_body_exited)
 	damage_interval_timer.timeout.connect(on_damage_interval_timer_timeout)
+	health_component.health_changed.connect(on_health_changed)
+	update_health_display()
 
 
 func _process(delta):
@@ -34,9 +38,13 @@ func check_deal_damage():
 	if number_colliding_body == 0 || !damage_interval_timer.is_stopped():
 		return
 
-	$HealthComponent.damage(1)
+	health_component.damage(1)
 	damage_interval_timer.start()
-	print($HealthComponent.current_health)
+	print(health_component.current_health)
+
+
+func update_health_display():
+	health_bar.value = health_component.get_health_percent()
 
 
 func on_damage_interval_timer_timeout():
@@ -50,3 +58,7 @@ func on_body_entered(other_body: Node2D):
 
 func on_body_exited(other_body: Node2D):
 	number_colliding_body -= 1
+
+
+func on_health_changed():
+	update_health_display()
