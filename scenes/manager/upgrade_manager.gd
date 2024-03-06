180cec7cd11ba339387486ea3776593b5ac8388f
Marllon Freitas <71530690+Marllon-Freitas@users.noreply.github.com>
2024-03-06 17:52:00 -0300

    feat: letting the player choose the axe as an ability

--- scenes/manager/upgrade_manager.gd
+++ scenes/manager/upgrade_manager.gd
@@ -1,5 +1,7 @@
 extends Node
 
+const QUANTITY_OF_UPGRADES_TO_CHOOSE = 2
+
 @export var upgrade_pool: Array[AbilityUpgrade]
 @export var experience_manager: Node
 @export var upgrade_screen_scene: PackedScene
@@ -9,17 +11,6 @@ var current_upgrades = {}
 
 func _ready():
 	experience_manager.level_up.connect(on_level_up)
-
-
-func on_level_up(current_level: int):
-	var chosen_upgrade = upgrade_pool.pick_random()
-	if chosen_upgrade == null:
-		return
-
-	var upgrade_screen_instance = upgrade_screen_scene.instantiate()
-	add_child(upgrade_screen_instance)
-	upgrade_screen_instance.set_ability_upgrades([chosen_upgrade] as Array[AbilityUpgrade])
-	upgrade_screen_instance.upgrade_selected.connect(on_upgrade_selected)
 
 
 func aplly_upgrade(upgrade: AbilityUpgrade):
@@ -32,5 +23,26 @@ func aplly_upgrade(upgrade: AbilityUpgrade):
 	GameEvents.emit_ability_upgrade_added(upgrade, current_upgrades)
 
 
+func pick_upgrades():
+	var chosen_upgrades: Array[AbilityUpgrade] = []
+	var filtered_upgrades = upgrade_pool.duplicate()
+	for i in QUANTITY_OF_UPGRADES_TO_CHOOSE:
+		var chosen_upgrade = filtered_upgrades.pick_random() as AbilityUpgrade
+		chosen_upgrades.append(chosen_upgrade)
+		filtered_upgrades = filtered_upgrades.filter(
+			func(upgrade): return upgrade.id != chosen_upgrade.id
+		)
+
+	return chosen_upgrades
+
+
 func on_upgrade_selected(upgrade: AbilityUpgrade):
 	aplly_upgrade(upgrade)
+
+
+func on_level_up(current_level: int):
+	var upgrade_screen_instance = upgrade_screen_scene.instantiate()
+	add_child(upgrade_screen_instance)
+	var chosen_upgrades = pick_upgrades()
+	upgrade_screen_instance.set_ability_upgrades(chosen_upgrades as Array[AbilityUpgrade])
+	upgrade_screen_instance.upgrade_selected.connect(on_upgrade_selected)
