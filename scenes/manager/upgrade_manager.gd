1a06d7a0842cea5305d4e763007bd614a0e6aa3f
Marllon Freitas <71530690+Marllon-Freitas@users.noreply.github.com>
2024-03-06 18:16:25 -0300

    fix: preventing the player from choose abilities twice

--- scenes/manager/upgrade_manager.gd
+++ scenes/manager/upgrade_manager.gd
@@ -20,6 +20,13 @@ func aplly_upgrade(upgrade: AbilityUpgrade):
 	else:
 		current_upgrades[upgrade.id]["quantity"] += 1
 
+	if upgrade.max_quantity > 0:
+		var current_quantity = current_upgrades[upgrade.id]["quantity"]
+		if current_quantity == upgrade.max_quantity:
+			upgrade_pool = upgrade_pool.filter(
+				func(pool_upgrade): return pool_upgrade.id != upgrade.id
+			)
+
 	GameEvents.emit_ability_upgrade_added(upgrade, current_upgrades)
 
 
@@ -28,6 +35,8 @@ func pick_upgrades():
 	var filtered_upgrades = upgrade_pool.duplicate()
 	for i in QUANTITY_OF_UPGRADES_TO_CHOOSE:
 		var chosen_upgrade = filtered_upgrades.pick_random() as AbilityUpgrade
+		if chosen_upgrade == null:
+			break
 		chosen_upgrades.append(chosen_upgrade)
 		filtered_upgrades = filtered_upgrades.filter(
 			func(upgrade): return upgrade.id != chosen_upgrade.id
@@ -40,7 +49,7 @@ func on_upgrade_selected(upgrade: AbilityUpgrade):
 	aplly_upgrade(upgrade)
 
 
-func on_level_up(current_level: int):
+func on_level_up(_current_level: int):
 	var upgrade_screen_instance = upgrade_screen_scene.instantiate()
 	add_child(upgrade_screen_instance)
 	var chosen_upgrades = pick_upgrades()
