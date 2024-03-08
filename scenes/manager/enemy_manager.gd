f91462a6f05e74e909412a0627001278edd0078f
Marllon Freitas <71530690+Marllon-Freitas@users.noreply.github.com>
2024-03-07 21:49:32 -0300

    spawning the new type of enemy

--- scenes/manager/enemy_manager.gd
+++ scenes/manager/enemy_manager.gd
@@ -5,14 +5,17 @@ const TERRAIN_LAYER_MASK = 1
 const DIRECTIONS_TO_CHECK = 4
 
 @export var basic_enemy_scene: PackedScene
+@export var wizard_enemy_scene: PackedScene
 @export var arena_time_manager: Node
 
 @onready var timer = $Timer
+var enemy_table = WeightedTable.new()
 
 var base_spawn_time = 0
 
 
 func _ready():
+	enemy_table.add_item(basic_enemy_scene, 10)
 	base_spawn_time = timer.wait_time
 	timer.timeout.connect(on_timer_timeout)
 	arena_time_manager.arena_difficulty_increased.connect(on_arena_difficulty_increased)
@@ -49,9 +52,11 @@ func on_timer_timeout():
 	if player == null:
 		return
 
-	var enemy: Node2D = basic_enemy_scene.instantiate()
-	var enitird_layer = get_tree().get_first_node_in_group("entities_layer")
-	enitird_layer.add_child(enemy)
+	var enemy_scene = enemy_table.pick_item()
+	var enemy: Node2D = enemy_scene.instantiate()
+
+	var enities_layer = get_tree().get_first_node_in_group("entities_layer")
+	enities_layer.add_child(enemy)
 	enemy.global_position = get_spawn_position()
 
 
@@ -59,3 +64,6 @@ func on_arena_difficulty_increased(arena_difficulty: int):
 	var time_off = (0.1 / 12) * arena_difficulty
 	time_off = min(time_off, 0.7)
 	timer.wait_time = base_spawn_time - time_off
+
+	if arena_difficulty == 6:
+		enemy_table.add_item(wizard_enemy_scene, 20)
