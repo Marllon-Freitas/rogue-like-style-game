4b606641e61bc6546bdadc07e2af53c58824de42
Marllon Freitas <71530690+Marllon-Freitas@users.noreply.github.com>
2024-03-07 21:32:15 -0300

    feat: adding a new type of enemy

--- scenes/game_object/basic_enemy/basic_enemy.gd
+++ scenes/game_object/basic_enemy/basic_enemy.gd
@@ -1,15 +1,16 @@
 extends CharacterBody2D
 
-const MAX_SPEED: int = 50
+@onready var visuals = $Visuals
+@onready var velocity_component = $VelocityComponent
 
-@onready var health_component: HealthComponent = $HealthComponent
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	var direction: Vector2 = get_directionToPlayer()
-	velocity = direction * MAX_SPEED
-	move_and_slide()
+	velocity_component.accelerate_to_player()
+	velocity_component.move(self)
+
+	var move_sign = sign(velocity.x)
+	if move_sign != 0:
+		visuals.scale = Vector2(-move_sign, 1)
 
 
 func get_directionToPlayer():
