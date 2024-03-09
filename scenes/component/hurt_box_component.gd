2adce2bffbeed3d606cf8fc6996d5040b712611d
Marllon Freitas <71530690+Marllon-Freitas@users.noreply.github.com>
2024-03-09 10:11:23 -0300

    feat: adding floating text to show damage

--- scenes/component/hurt_box_component.gd
+++ scenes/component/hurt_box_component.gd
@@ -3,16 +3,25 @@ class_name HurtBoxComponent
 
 @export var health_component: HealthComponent
 
+var floating_text_scene = preload("res://scenes/ui/floating_text.tscn")
+
+
 func _ready():
-  area_entered.connect(on_area_entered)
+	area_entered.connect(on_area_entered)
 
 
 func on_area_entered(other_area: Area2D):
-  if not other_area is HitBoxComponent:
-    return
-  
-  if health_component == null:
-    return
+	if not other_area is HitBoxComponent:
+		return
 
-  var hit_box_component = other_area as HitBoxComponent
-  health_component.damage(hit_box_component.damage)
+	if health_component == null:
+		return
+
+	var hit_box_component = other_area as HitBoxComponent
+	health_component.damage(hit_box_component.damage)
+
+	var floating_text = floating_text_scene.instantiate() as Node2D
+	get_tree().get_first_node_in_group("foreground_layer").add_child(floating_text)
+
+	floating_text.global_position = global_position + (Vector2.UP * 16)
+	floating_text.start(str(hit_box_component.damage))
