473556b74542771219a8c11e865db32e6e7d9e37
Marllon Freitas <71530690+Marllon-Freitas@users.noreply.github.com>
2024-03-10 22:16:35 -0300

    feat: animating the upgrade card

--- scenes/ui/ability_upgrade_card.gd
+++ scenes/ui/ability_upgrade_card.gd
@@ -5,9 +5,19 @@ signal selected
 @onready var name_label: Label = $%NameLabel
 @onready var description_label: Label = $%DescriptionLabel
 
+var disable: bool = false
+
 
 func _ready():
 	gui_input.connect(on_gui_input)
+	mouse_entered.connect(on_mouse_entered)
+
+
+func play_in(delay: float = 0):
+	modulate = Color.TRANSPARENT
+	scale = Vector2.ZERO
+	await get_tree().create_timer(delay).timeout
+	$AnimationPlayer.play("in")
 
 
 func set_ability_upgrade(upgrade: AbilityUpgrade):
@@ -16,5 +26,28 @@ func set_ability_upgrade(upgrade: AbilityUpgrade):
 
 
 func on_gui_input(event: InputEvent):
+	if disable:
+		return
 	if event.is_action_pressed("left_click"):
-		selected.emit()
+		select_card()
+
+
+func play_discard():
+	$AnimationPlayer.play("discard")
+
+
+func select_card():
+	disable = true
+	$AnimationPlayer.play("selected")
+	for other_cards in get_tree().get_nodes_in_group("upgrade_card"):
+		if other_cards == self:
+			continue
+		other_cards.play_discard()
+	await $AnimationPlayer.animation_finished
+	selected.emit()
+
+
+func on_mouse_entered():
+	if disable:
+		return
+	$HoverAnimationPlayer.play("hover")
